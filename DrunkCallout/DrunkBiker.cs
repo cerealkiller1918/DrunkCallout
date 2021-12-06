2d6ed81686dda3fae338de88b3f8dd10ceaadb33
Justin Frasier <justinfrasier@killermediagroup.com>
2021-12-06 14:00:30 -0600

    its just fucked had to og back the old way

--- DrunkCallout/DrunkBiker.cs
+++ DrunkCallout/DrunkBiker.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
 using CitizenFX.Core;
@@ -12,10 +13,14 @@ namespace DrunkCallout
     public class DrunkBiker : Callout
     {
         private Ped _driver;
+        private Random rnd = new Random();
 
-        public DrunkBiker()
+            public DrunkBiker()
         {
-            InitInfo(World.GetNextPositionOnStreet(Game.PlayerPed.GetOffsetPosition(OffSets.RandomOffSet())));
+            float x = rnd.Next(100, 700);
+            float y = rnd.Next(100, 700);
+            Vector3 offset = new Vector3(x, y, 0);
+            InitInfo(World.GetNextPositionOnStreet(Game.PlayerPed.GetOffsetPosition(offset)));
             ShortName = "Drunk Biker";
             CalloutDescription = "A person is riding intoxicated";
             ResponseCode = 3;
@@ -29,14 +34,34 @@ namespace DrunkCallout
             // need to add a list of bike to chose from.
             Vehicle bike = await SpawnVehicle(VehicleHash.TriBike, Location);
 
-            Intoxicated.SetUpIntoxicatedPed(_driver);
+            String[] items = {"Beer", "Wine", "Whiskey", "Moon shine"}; 
+            PedData data = new PedData();
+
+            List<Item> list = new List<Item>();
+            float blood = rnd.Next(80, 150);
+            
+            data.BloodAlcoholLevel = (float)Math.Round(blood/100,2);
+            var itemName2 = items[rnd.Next(items.Length)];
+            Item item2 = new Item()
+            {
+                Name = itemName2,
+                IsIllegal = false
+            };
+            list.Add(item2);
+            data.Items = list;
+            Utilities.SetPedData(_driver.NetworkId, data);
+
+            _driver.AlwaysKeepTask = true;
+            _driver.BlockPermanentEvents = true;
+            API.SetPedIsDrunk(_driver.GetHashCode(),true);
+            _driver.AttachBlip();
 
             PlayerData playerData = Utilities.GetPlayerData();
             string displayName = playerData.DisplayName;
             API.SetDriveTaskMaxCruiseSpeed(_driver.GetHashCode(), 35f);
             API.SetDriveTaskDrivingStyle(_driver.GetHashCode(), 524852);
             _driver.Task.FleeFrom(player);
-            Killer.Notify("~o~Officer ~b~" + displayName + ",~o~ the biker is fleeing!");
+            Notify("~o~Officer ~b~" + displayName + ",~o~ the biker is fleeing!");
             bike.AttachBlip();
         }
 
@@ -46,8 +71,21 @@ namespace DrunkCallout
             PedData pedData = await Utilities.GetPedData(_driver.NetworkId);
             string firstname = pedData.FirstName;
             API.Wait(60000);
-            Killer.DrawSubtitle("~r~[" + firstname + "] ~s~Are those police lights?", 5000);
+            DrawSubtitle("~r~[" + firstname + "] ~s~Are those police lights?", 5000);
             UpdateData();
+        }
+        
+        private static void Notify(string message)
+        {
+            API.BeginTextCommandThefeedPost("STRING");
+            API.AddTextComponentSubstringPlayerName(message);
+            API.EndTextCommandThefeedPostTicker(false, true);
+        }
+        private static void DrawSubtitle(string message, int duration)
+        {
+            API.BeginTextCommandPrint("STRING");
+            API.AddTextComponentSubstringPlayerName(message);
+            API.EndTextCommandPrint(duration, false);
         }
 
     }
--- DrunkCallout/DrunkFight.cs
+++ DrunkCallout/DrunkFight.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using CitizenFX.Core;
+using CitizenFX.Core.Native;
 using FivePD.API;
 using FivePD.API.Utils;
 
@@ -9,9 +11,13 @@ namespace DrunkCallout
     [CalloutProperties("Drunk Fight","Cerealkiller1918","0.0.1")]
     public class DrunkFight : Callout
     {
+        Random rnd = new Random();
         public DrunkFight()
         {
-            InitInfo(World.GetNextPositionOnStreet(Game.PlayerPed.GetPositionOffset(OffSets.RandomOffSet())));
+            float x = rnd.Next(100, 700);
+            float y = rnd.Next(100, 700);
+            Vector3 offset = new Vector3(x, y, 0);
+            InitInfo(World.GetNextPositionOnStreet(Game.PlayerPed.GetPositionOffset(offset)));
 
             ShortName = "Drunk Fight";
             CalloutDescription = " A fight had broken out with intoxicated people";
@@ -25,8 +31,50 @@ namespace DrunkCallout
             Ped suspect1 = await SpawnPed(RandomUtils.GetRandomPed(), Location + 2);
             Ped suspect2 = await SpawnPed(RandomUtils.GetRandomPed(), Location + 2);
             
-            Intoxicated.SetUpIntoxicatedPed(suspect1);
-            Intoxicated.SetUpIntoxicatedPed(suspect2);;
+            String[] items = {"Beer", "Wine", "Whiskey", "Moon shine"};
+            
+            PedData data1 = new PedData();
+
+            List<Item> list1 = new List<Item>();
+            float blood1 = rnd.Next(80, 150);
+            
+            data1.BloodAlcoholLevel = (float)Math.Round(blood1/100,2);
+            var itemName1 = items[rnd.Next(items.Length)];
+            Item item1 = new Item()
+            {
+                Name = itemName1,
+                IsIllegal = false
+            };
+            list1.Add(item1);
+            data1.Items = list1;
+            Utilities.SetPedData(suspect1.NetworkId, data1);
+
+            suspect1.AlwaysKeepTask = true;
+            suspect1.BlockPermanentEvents = true;
+            API.SetPedIsDrunk(suspect1.GetHashCode(),true);
+            suspect1.AttachBlip();
+           
+          ////////////////////////////////
+            PedData data2 = new PedData();
+
+            List<Item> list2 = new List<Item>();
+            float blood2 = rnd.Next(80, 150);
+            
+            data2.BloodAlcoholLevel = blood2/100;
+            var itemName2 = items[rnd.Next(items.Length)];
+            Item item2 = new Item()
+            {
+                Name = itemName2,
+                IsIllegal = false
+            };
+            list2.Add(item2);
+            data2.Items = list2;
+            Utilities.SetPedData(suspect2.NetworkId, data2);
+
+            suspect2.AlwaysKeepTask = true;
+            suspect2.BlockPermanentEvents = true;
+            API.SetPedIsDrunk(suspect2.GetHashCode(),true);
+            suspect2.AttachBlip();
             
             suspect1.Task.FightAgainst(suspect2);
             suspect2.Task.FightAgainst(suspect1);
