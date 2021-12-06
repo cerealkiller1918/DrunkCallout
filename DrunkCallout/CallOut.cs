004a0e043c184c58b47547777c0d8712bd6989f1
Justin Frasier <justinfrasier@killermediagroup.com>
2021-12-05 23:34:31 -0600

    Added a Callout.cs

---
+++ DrunkCallout/CallOut.cs
@@ -0,0 +1,20 @@
+using CitizenFX.Core.Native;
+
+namespace DrunkCallout
+{
+    public static class CallOut
+    {
+        private static void Notify(string message)
+        {
+            API.BeginTextCommandThefeedPost("STRING");
+            API.AddTextComponentSubstringPlayerName(message);
+            API.EndTextCommandThefeedPostTicker(false, true);
+        }
+        private static void DrawSubtitle(string message, int duration)
+        {
+            API.BeginTextCommandPrint("STRING");
+            API.AddTextComponentSubstringPlayerName(message);
+            API.EndTextCommandPrint(duration, false);
+        }
+    }
+}
